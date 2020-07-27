2064aa724e31e1ceb1fbbc3d900aac1fa08a0bd4
O-A-E <67084069+O-A-E@users.noreply.github.com>
2020-07-27 10:17:33 -0600

    updated with render and return methods

--- src/App.js
+++ src/App.js
@@ -3,7 +3,25 @@ import React from 'react';
 
 class App extends React.Component
 {
-  return 
+  render()
+  {  
+  return (
+        <>
+        <h1> React To Do App </h1>
+        <form>
+          <label htmlFor ="newToDo">
+            Enter a new "To-Do:"
+            <input type ="text" name = "newToDo" id ="newToDo" required />
+          </label>
+          <input type = "submit" value= "Add New To-Do"/>
+        </form>
+        <h2> Current To-Dos:</h2>
+        <ul>
+          
+        </ul>
+      </>
+      );
+    }
 }
 
 export default App;
